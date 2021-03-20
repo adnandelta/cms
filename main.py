cd9a93590df29032814f740352100f1a1b2efe60
umz <kumraz858gmail.com>
2021-03-21 04:58:36 +0530

    update

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 from flask import Flask, redirect, url_for, request, render_template
+from pymongo import MongoClient
 
 
 app = Flask(__name__, static_url_path='/static')
+#client = MongoClient('')
+#db = client['cms']
+db = None
 
-
-username = "asd"
 
 def validate(email, password):
-    return False
+    user = db['users'].findOne({'email':email})
+
+    if user is None:
+        return False
+    
+    if user['password'] != password:
+        return False
+    
+    return True
 
 
 @app.route('/')
@@ -16,6 +26,41 @@ def index():
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
+    if request.method == "POST":
+        name = request.form['name']
+        email = request.form['email']
+        password = request.form['password']
+
+        data = {
+            "name":name,
+            "email":email,
+            "password":password,
+            "section": 1,
+            "professor": {
+                "name":"Daniel",
+                "email":"daniel@xyz.com"
+            },
+            "assignments": {
+                "maths":False,
+                "english":False,
+                "physics":False
+            },
+            "exam": {
+                "maths":0,
+                "english":0,
+                "physics":0
+            }
+            "attendance": {
+                "maths":10,
+                "english":10,
+                "physics":10
+            },
+            "prof":False
+        }
+
+        db['users'].insertOne(data)
+        return url_for('login')
+    
     return render_template('register.html')
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -26,6 +71,7 @@ def login():
         email = request.form['email']
         password = request.form['password']
         if validate(email, password):
+            user = db['users'].findOne({'email':email})
             return url_for('dashboard', userobj=user)
         else:
             return render_template('login.html', creds=False)
@@ -33,7 +79,8 @@ def login():
     return render_template('login.html', creds=creds)
 
 @app.route('/dashboard')
-def dashboard(userobj):
+def dashboard(userobj=None):
+    userobj = None
     return render_template('dashboard.html', user=userobj)
 
 if __name__ == "__main__":
